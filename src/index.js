4c25e168883e473e377f1ffa749112e51b002c79
Anna Lednyova <ashtart@gmail.com>
2023-07-17 16:39:20 +0300

    export-import continue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from './components/Counter';
+// import MyMath from './components/Math';
+import * as MyMath from './components/Math';
+
+console.log(MyMath);
 
 const div = document.querySelector('#root');
 
